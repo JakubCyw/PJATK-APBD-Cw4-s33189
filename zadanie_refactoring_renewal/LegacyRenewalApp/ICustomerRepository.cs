c60b06cf7a8b3cc05302883c4b7635d87df1dba1
s33189 <s33189@pjwstk.edu.pl>
2026-04-08 11:45:31 +0200

    ICustomerRepository class add

---
+++ zadanie_refactoring_renewal/LegacyRenewalApp/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace LegacyRenewalApp;
+
+public interface ICustomerRepository
+{
+    Customer GetByID(int id);
+}
+
+public interface IPlanRepository
+{
+    SubscriptionPlan GetByCode(string code);
+}
