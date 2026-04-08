c6230f30d0062179b0fb9c703f24da9cb5fdf293
s33189 <s33189@pjwstk.edu.pl>
2026-04-08 11:42:53 +0200

    IBillingService class add

---
+++ zadanie_refactoring_renewal/LegacyRenewalApp/IBillingService.cs
@@ -0,0 +1,13 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IBillingService
+{
+    void SaveInvoice(RenewalInvoice invoice);
+    void SendEmail(string email, string subject, string body);
+}
+
+public class LegacyBillingServiceWrapper : IBillingService
+{
+    public void  SaveInvoice(RenewalInvoice invoice) => LegacyBillingGateway.SaveInvoice(invoice);
+    public void SendEmail(string email, string subject, string body) => LegacyBillingGateway.SendEmail(email, subject, body);
+}
